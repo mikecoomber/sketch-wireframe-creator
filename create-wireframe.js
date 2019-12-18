60d53fe466f58ed234b4286ef52cd9f61e42de98
mikecoomber <58986130+mikecoomber@users.noreply.github.com>
2019-12-18 17:40:33 +0000

    Styling refinements
    Started adding support for targeting individual component types.
    Updated colour correction to use a thresholding algorithm.

--- create-wireframe.js
+++ create-wireframe.js
@@ -1,5 +1,45 @@
 import sketch from 'sketch';
 
+const componentFamilies = ['Mobile', 'Icon'];
+
+const componentStyles = new Map();
+
+componentStyles.set('Button', 
+  function(layer) {
+    layer.style.fills=[createFill('#ffffff')];
+    layer.style.borders=[blackOutline()];
+  }
+)
+componentStyles.set('Background', 
+  function(layer) {
+    layer.style.fills=[createFill('#ffffff')];
+  }
+)
+componentStyles.set('Icon',
+  function(layer) {
+    if (layer.name !== 'Rectangle') { 
+      let color = layer.style.fills[0].color;
+      let newColor = getNewColor(color);
+      layer.style.fills = [createFill(newColor)];
+    }
+  }
+)
+
+function detatchSymbols(layers) {
+  layers.forEach(function(layer) {
+		if (layer.layers) {
+			detatchSymbols(layer.layers);
+		}
+		else {
+			if(layer.type == 'SymbolInstance'){
+				layer.detach({
+					recursively: true
+				});
+			}
+    }
+  });
+}
+
 export default function(context) {
   let sketch = require('sketch');
   
@@ -18,49 +58,44 @@ export default function(context) {
   }
 }
 
-function findBaseLayer(layers) {
+function findBaseLayer(layers, currentComponent) {
   layers.forEach( function (layer, i) {
     if (layer.type === 'Group' || layer.type === 'Artboard') {
-      findBaseLayer(layer.layers);
+      if (stringContainsArrayContents(layer.name, componentFamilies)) {
+        currentComponent = getComponentType(layer.name)
+      }
+      findBaseLayer(layer.layers, currentComponent);
     } else {
-      changeLayerColour(layer);
+      changeLayerStyle(layer, currentComponent);
     }
   })
 } 
 
-function changeLayerColour(layer) {
-  if (layer.type === 'ShapePath') {
+function changeLayerStyle(layer, currentComponent) {
+  if (layer.type === 'Text') {
+    let color = layer.style.textColor;
+    layer.style.textColor = currentComponent === 'Button' ? '#000000' : getNewColor(color);
+  } else if (componentStyles.get(currentComponent)) {
+    const setStyle = componentStyles.get(currentComponent);
+    setStyle(layer);
+  } else if (layer.type === 'ShapePath') {
     let color = layer.style.fills.length > 0 ? layer.style.fills[0].color : '#000000';
     let newColor = getNewColor(color);
     layer.style.fills = [createFill(newColor)];
     if (layer.name === 'Rectangle') {
       layer.style.borders = [blackOutline()];
     }
-  } else if (layer.type === 'Text') {
-    let color = layer.style.textColor;
-    layer.style.textColor = getNewColor(color);
-    
-  }
+  } 
 }
 
 function getNewColor(color) {
-  let colorInt = parseInt(color.slice(1, color.length - 1), 16);
-  return colorInt < 134217727 ? '#000000' : '#ffffff';
-}
-
-function detatchSymbols(layers) {
-  layers.forEach(function(layer) {
-		if (layer.layers) {
-			detatchSymbols(layer.layers);
-		}
-		else {
-			if(layer.type == 'SymbolInstance'){
-				layer.detach({
-					recursively: true
-				});
-			}
-    }
-  });
+  console.log(color)
+  let r = parseInt(color.substring(1, 3), 16);
+  let g = parseInt(color.substring(3, 5), 16);
+  let b = parseInt(color.substring(5, 7), 16);
+  let avg = (r + b + g) / 3
+  console.log(avg)
+  return avg < 128 ? '#000000' : '#ffffff';
 }
 
 function createFill(color) {
@@ -76,4 +111,29 @@ function blackOutline() {
     fillType: 'Color',
     color: '#000000',
   };
+}
+
+function stringContainsArrayContents(string, array) {
+  let res = false;
+  array.forEach(function (item, i) {
+    if (string.includes(item)) {
+      res = true
+    }
+  })
+  return res
+}
+
+function getComponentType(componentName) {
+  const start = componentName.indexOf('/') + 2
+  let end = start;
+  let result = ''
+  if (componentName.includes('Mobile')){
+    while (componentName[end] !== '/') {
+      end++;
+    } 
+    result = componentName.substring(start, end - 1)
+  } else if (componentName.includes('Icon')) {
+    result = componentName.substring(0, componentName.indexOf('/') - 1)
+  }
+  return result
 }
